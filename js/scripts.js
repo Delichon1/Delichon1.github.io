764da7967a31f632cd86fc98e92572820ace0877
Delichon1 <mttm1@i.ua>
2025-07-14 22:15:03 +0200

    Update scripts.js
    Website. 1st edition

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-/*!
-* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Скрипт для раскрытия/скрытия аккордеонов в секции Skills & Tools
+document.querySelectorAll('.faq-item').forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('faq-active');
+  });
+});
+
+//код, который заполнит полосы при загрузке
+document.querySelectorAll('.progress-bar').forEach(bar => {
+  const value = bar.getAttribute('aria-valuenow');
+  bar.style.width = value + '%';
+});
